b15bdea547d0d119d9b1e0adecadf3c97293e78e
eduardo <171377664+barbosa-eduardo@users.noreply.github.com>
2024-06-15 20:19:06 -0300

    Add function to get human move

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ function getComputerChoice() {
     return options[parseInt(Math.random() * 3)];
 }
 
+function getHumanChoice() {
+    let choice = prompt("Make your move! Rock, paper or scissors?").toLowerCase();
+    if (!options.includes(choice)) {
+        alert("Error! Player must type rock, paper or scissors");
+        return getHumanChoice();
+    }
+    return choice;
+}
