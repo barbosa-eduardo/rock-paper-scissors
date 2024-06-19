295b37c4399181941365f6a17f4d19780ab7475e
eduardo <171377664+barbosa-eduardo@users.noreply.github.com>
2024-06-19 16:18:22 -0300

    Add end game condition and logic

--- script.js
+++ script.js
@@ -7,34 +7,36 @@ buttons.forEach((btn) => {
 
 let options = ["rock", "paper", "scissors"];
 let computerScore = 0;
-let humanScore = 0;
+let playerScore = 0;
 
 function getComputerChoice() {
     return options[parseInt(Math.random() * 3)];
 }
 
-function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
-    if (humanChoice == computerChoice) {
+function playRound(playerChoice, computerChoice) {
+    playerChoice = playerChoice.toLowerCase();
+    if (playerChoice == computerChoice) {
         displayResult("It is a TIE! No one wins.");
-    } else if ((humanChoice == "rock" && computerChoice == "scissors") || (humanChoice == "paper" && computerChoice == "rock") || (humanChoice == "scissors" && computerChoice == "paper")) {
-        humanScore++;
-        displayResult(`You win! ${humanChoice} beats ${computerChoice}`);
+    } else if ((playerChoice == "rock" && computerChoice == "scissors") || (playerChoice == "paper" && computerChoice == "rock") || (playerChoice == "scissors" && computerChoice == "paper")) {
+        playerScore++;
+        displayResult(`You win! ${playerChoice} beats ${computerChoice}`);
         updateScore();
     } else {
         computerScore++;
-        displayResult(`You lose! ${computerChoice} beats ${humanChoice}`);
+        displayResult(`You lose! ${computerChoice} beats ${playerChoice}`);
         updateScore();
     }
+
+    checkWinner();
 }
 
 function playGame() {
-    if (humanScore > computerScore) {
-        alert("Human wins with " + humanScore + " points!");
-    } else if (computerScore > humanScore) {
+    if (playerScore > computerScore) {
+        alert("Human wins with " + playerScore + " points!");
+    } else if (computerScore > playerScore) {
         alert("Computer wins with " + computerScore + " points!");
     } else {
-        alert("It is a tie! Both players got " + humanScore);
+        alert("It is a tie! Both players got " + playerScore);
     }
 }
 
@@ -50,6 +52,28 @@ function updateScore() {
     const player = document.querySelector(".player_score");
     const computer = document.querySelector(".computer_score");
 
-    player.innerText = humanScore;
+    player.innerText = playerScore;
     computer.innerText = computerScore;
+}
+
+function checkWinner() {
+    if (playerScore >= 5) {
+        alert("Player won the game!");
+        resetGame();
+    } else if (computerScore >= 5) {
+        alert("Computer won the game!");
+        resetGame();
+    }
+}
+
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    updateScore();
+
+    const resultList = document.querySelector("ul").childNodes;
+
+    for (let i = resultList.length - 1; i >= 0; i--) {
+        resultList[i].remove();
+    }
 }
